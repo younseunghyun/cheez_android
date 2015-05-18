9c1136502968e4ec8fc96a2a6cc73c38f3439e7a
jiho <polaris341@gmail.com>
2015-05-18 18:54:51 +0900

    업로드 화면 - og 가져오는 부분까지 됨

--- app/src/main/java/co/cheez/cheez/http/listener/DefaultErrorListener.java
+++ app/src/main/java/co/cheez/cheez/http/listener/DefaultErrorListener.java
@@ -1,7 +1,5 @@
 package co.cheez.cheez.http.listener;
 
-import android.util.Log;
-
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 
@@ -12,6 +10,5 @@ public class DefaultErrorListener implements Response.ErrorListener {
     @Override
     public void onErrorResponse(VolleyError error) {
         // TODO : default error handling
-        Log.e("error", error.toString());
     }
 }
